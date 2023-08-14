88fac245a6cd5b4efad88a44c34e4fe7441e8023
mdaffa48 <work@muhammaddaffa.com>
2023-08-14 10:04:10 +0700

    Add sellwand

---
+++ src/main/java/com/muhammaddaffa/nextgens/sellwand/Sellwand.java
@@ -0,0 +1,136 @@
+package com.muhammaddaffa.nextgens.sellwand;
+
+import com.muhammaddaffa.nextgens.NextGens;
+import com.muhammaddaffa.nextgens.hooks.vault.VaultEconomy;
+import com.muhammaddaffa.nextgens.utils.*;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.UUID;
+
+public class Sellwand {
+
+    public static boolean action(Player player, ItemStack stack, Inventory inventory) {
+        // scrap the data from the item
+        ItemBuilder builder = new ItemBuilder(stack);
+        ItemMeta meta = builder.getItemMeta();
+        Double multiplier = meta.getPersistentDataContainer().get(NextGens.sellwand_multiplier, PersistentDataType.DOUBLE);
+        Integer uses = meta.getPersistentDataContainer().get(NextGens.sellwand_uses, PersistentDataType.INTEGER);
+        Double totalSold = meta.getPersistentDataContainer().get(NextGens.sellwand_total_sold, PersistentDataType.DOUBLE);
+        Integer totalItemsSold = meta.getPersistentDataContainer().get(NextGens.sellwand_total_items, PersistentDataType.INTEGER);
+        // if one of the data is null, skip it
+        if (multiplier == null || uses == null || totalSold == null || totalItemsSold == null) {
+            return false;
+        }
+        double totalValue = 0.0;
+        int totalItems = 0;
+        // loop through inventory contents
+        for (ItemStack item : inventory) {
+            // get value
+            double value = Utils.getPriceValue(player, item);
+            // if the item has value, register it
+            if (value > 0) {
+                totalItems += item.getAmount();
+                totalValue += value;
+                // remove the item
+                item.setAmount(0);
+            }
+        }
+        // check if player has anything to sell
+        if (totalItems == 0) {
+            // send message
+            Common.config(player, "messages.no-sell");
+            // play bass sound
+            Common.playBassSound(player);
+            return true;
+        }
+        // apply the multiplier
+        final double afterMultiplier = totalValue * multiplier;
+        // deposit the money
+        VaultEconomy.deposit(player, afterMultiplier);
+        // send the visual action
+        VisualAction.send(player, Config.CONFIG.getConfig(), "sell-options", new Placeholder()
+                .add("{amount}", Common.digits(totalItems))
+                .add("{value}", Common.digits(afterMultiplier)));
+        // final uses
+        int finalUses = uses - 1;
+        // if the final uses is 0
+        if (finalUses == 0) {
+            // destroy the item
+            stack.setAmount(0);
+            // send message
+            Common.config(player, "messages.sellwand-broke");
+            // play break sound
+            player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0f, 1.0f);
+            return true;
+        }
+        // set back the pdc
+        if (uses > 0) {
+            builder.pdc(NextGens.sellwand_uses, finalUses);
+        }
+        builder.pdc(NextGens.sellwand_total_sold, totalSold + totalValue);
+        builder.pdc(NextGens.sellwand_total_items, totalItemsSold + totalItems);
+        // update the item
+        update(builder.build());
+        return true;
+    }
+
+    public static void update(ItemStack stack) {
+        // update the item
+        ItemBuilder builder = new ItemBuilder(stack);
+        // get the multiplier and uses
+        ItemMeta meta = builder.getItemMeta();
+        Double multiplier = meta.getPersistentDataContainer().get(NextGens.sellwand_multiplier, PersistentDataType.DOUBLE);
+        Integer uses = meta.getPersistentDataContainer().get(NextGens.sellwand_uses, PersistentDataType.INTEGER);
+        Double totalSold = meta.getPersistentDataContainer().get(NextGens.sellwand_total_sold, PersistentDataType.DOUBLE);
+        Integer totalItemsSold = meta.getPersistentDataContainer().get(NextGens.sellwand_total_items, PersistentDataType.INTEGER);
+        // if one of the data is null, skip it
+        if (multiplier == null || uses == null || totalSold == null || totalItemsSold == null) {
+            return;
+        }
+        // set the display name and lore back
+        builder.name(Config.CONFIG.getString("sellwand.item.display-name"));
+        builder.lore(Config.CONFIG.getStringList("sellwand.item.lore"));
+        builder.placeholder(new Placeholder()
+                .add("{multiplier}", Common.digits(multiplier))
+                .add("{uses}", getUsesPlaceholder(uses))
+                .add("{total_sold}", Common.digits(totalSold))
+                .add("{total_items}", Common.digits(totalItemsSold)));
+        // set the item meta
+        builder.build();
+    }
+
+    public static ItemStack create(double multiplier, int uses) {
+        // create the item
+        ItemBuilder builder = ItemBuilder.fromConfig(Config.CONFIG.getConfig(), "sellwand.item");
+        builder.pdc(NextGens.sellwand_global, UUID.randomUUID().toString());
+        builder.pdc(NextGens.sellwand_multiplier, multiplier);
+        builder.pdc(NextGens.sellwand_uses, uses);
+        builder.pdc(NextGens.sellwand_total_sold, 0D);
+        builder.pdc(NextGens.sellwand_total_items, 0);
+        builder.placeholder(new Placeholder()
+                .add("{multiplier}", Common.digits(multiplier))
+                .add("{uses}", getUsesPlaceholder(uses))
+                .add("{total_sold}", Common.digits(0))
+                .add("{total_items}", Common.digits(0)));
+
+        return builder.build();
+    }
+
+    public static String getUsesPlaceholder(int uses) {
+        if (uses == -1) {
+            return Config.CONFIG.getString("sellwand.unlimited-placeholder");
+        }
+        return Common.digits(uses);
+    }
+
+    public static boolean isSellwand(ItemStack stack) {
+        if (stack == null || stack.getItemMeta() == null) return false;
+        return stack.getItemMeta().getPersistentDataContainer().has(NextGens.sellwand_global, PersistentDataType.STRING);
+    }
+
+}
---
+++ src/main/java/com/muhammaddaffa/nextgens/sellwand/managers/SellwandListener.java
@@ -0,0 +1,49 @@
+package com.muhammaddaffa.nextgens.sellwand.managers;
+
+import com.griefcraft.lwc.LWC;
+import com.muhammaddaffa.nextgens.sellwand.Sellwand;
+import com.muhammaddaffa.nextgens.utils.Common;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class SellwandListener implements Listener {
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    private void onInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+        ItemStack stack = event.getItem();
+        // if item is not a sellwand, skip
+        if (!Sellwand.isSellwand(stack)) {
+            return;
+        }
+        if (block.getState() instanceof Container container) {
+            // LWC check
+            if (Bukkit.getPluginManager().getPlugin("LWC") != null && !LWC.getInstance().canAccessProtection(player, block)) {
+                // send a message and do nothing
+                Common.config(player, "messages.sellwand-failed");
+                // cancel the event
+                event.setCancelled(true);
+                // bass sound
+                Common.playBassSound(player);
+                return;
+            }
+            // try to sell the content of the chest
+            if (Sellwand.action(player, stack, container.getInventory())) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+}
