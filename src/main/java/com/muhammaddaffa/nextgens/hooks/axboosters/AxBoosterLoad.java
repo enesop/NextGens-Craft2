1504f46c16ff8ec25c27fc288094c23c4c70b402
HeadsShot <41201881+Starfruit2210@users.noreply.github.com>
2025-06-16 18:40:48 +0700

    add: AxBooster Hook

---
+++ src/main/java/com/muhammaddaffa/nextgens/hooks/axboosters/AxBoosterLoad.java
@@ -0,0 +1,26 @@
+package com.muhammaddaffa.nextgens.hooks.axboosters;
+
+import com.artillexstudios.axboosters.api.AxBoostersAPI;
+import com.artillexstudios.axboosters.api.events.AxBoostersLoadEvent;
+import com.muhammaddaffa.nextgens.NextGens;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class AxBoosterLoad implements Listener {
+
+    private final NextGens plugin;
+
+    public AxBoosterLoad(NextGens plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onLoad(AxBoostersLoadEvent event) {
+        AxBoosterSellListener sellHook = new AxBoosterSellListener();
+        AxBoosterSpeedListener speedHook = new AxBoosterSpeedListener();
+        plugin.getServer().getPluginManager().registerEvents(sellHook, plugin);
+        plugin.getServer().getPluginManager().registerEvents(speedHook, plugin);
+        AxBoostersAPI.registerBoosterHook(plugin, sellHook);
+        AxBoostersAPI.registerBoosterHook(plugin, speedHook);
+    }
+}
---
+++ src/main/java/com/muhammaddaffa/nextgens/hooks/axboosters/AxBoosterSpeedListener.java
@@ -0,0 +1,60 @@
+package com.muhammaddaffa.nextgens.hooks.axboosters;
+
+import com.artillexstudios.axboosters.boosters.BoosterManager;
+import com.artillexstudios.axboosters.hooks.booster.BoosterHook;
+import com.artillexstudios.axboosters.libs.kyori.adventure.key.Key;
+import com.artillexstudios.axboosters.users.User;
+import com.artillexstudios.axboosters.users.UserList;
+import com.muhammaddaffa.nextgens.api.events.generators.GeneratorGenerateItemEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class AxBoosterSpeedListener implements BoosterHook, Listener {
+
+    public static final AxBoosterSpeedListener INSTANCE = new AxBoosterSpeedListener();
+
+    @Override
+    public Key getKey() {
+        return Key.key("nextgens", "speed_multiplier");
+    }
+
+
+    @Override
+    public Material getIcon() {
+        return Material.GOLD_INGOT;
+    }
+
+    @Override
+    public boolean isPersistent() {
+        return true;
+    }
+
+    @EventHandler
+    public void onSpeed(@NotNull GeneratorGenerateItemEvent event) {
+        UUID uuid = event.getActiveGenerator().getOwner();
+        Player player = Bukkit.getPlayer(uuid);
+
+        if (player == null) return;
+
+        User user = UserList.getUser(player);
+        if (user == null) {
+            return;
+        }
+
+        float boost = user.getBoost(this);
+
+        if (boost == 1.0f) return;
+
+        double before = event.getTimer();
+        double boosted = BoosterManager.multiply(boost, before);
+
+        event.setTimer(boosted);
+    }
+}
